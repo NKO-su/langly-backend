5373529780e22dffff13ceaec8b16a99dda3ff4d
Le Quang Quyen <Lequangquyenlqq@gmail.com>
2026-08-13 14:16:34 +0700

    Initial commit: Spring Boot project setup with PostgreSQL connection

---
+++ src/main/java/com/langly/langly_backend/LanglyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.langly.langly_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanglyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanglyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/langly/langly_backend/LanglyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.langly.langly_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanglyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
