c1ceffd491ca3a477bb03564eb99348f75dda01f
Le Quang Quyen <Lequangquyenlqq@gmail.com>
2026-08-17 14:51:14 +0700

    feat: add User entity and AuthProvider enum

---
+++ src/main/java/model/User.java
@@ -0,0 +1,100 @@
+package model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    /**
+     * Note
+     * @Entity                  :Đại diện cho 1 bảng trong Database
+     * @Table(name = "users")   :Chỉ định rõ tên bảng trong Database
+     * @Id                      : Đánh dấu Private Key
+     *
+     * GeneratedValue(strategy = GenerationType.IDENTITY):Tự động tăng giá trị
+     * @Column(unique = true, nullable = false) :Cấu hình ràng buộc
+     * @Enumerated(EnumType.STRING): bắt buộc phải có khi field là kiểu enum
+     * updatable = false        :không cho phép update
+     */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = true)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AuthProvider provider;
+
+    @Column(nullable = true)
+    private String providerId;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    protected User(){}
+    public User(Long id, String email, String password, AuthProvider provider, String providerId, LocalDateTime createdAt) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.provider = provider;
+        this.providerId = providerId;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public AuthProvider getProvider() {
+        return provider;
+    }
+
+    public void setProvider(AuthProvider provider) {
+        this.provider = provider;
+    }
+
+    public String getProviderId() {
+        return providerId;
+    }
+
+    public void setProviderId(String providerId) {
+        this.providerId = providerId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
