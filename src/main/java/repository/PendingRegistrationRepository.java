09e3a8d60c2232a2955060867daa35f7f968c971
Le Quang Quyen <Lequangquyenlqq@gmail.com>
2026-08-18 13:41:57 +0700

    "feat: add PendingRegistrationRepository with findByEmail and findByToken"

---
+++ src/main/java/repository/PendingRegistrationRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import model.PendingRegistration;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PendingRegistrationRepository extends JpaRepository<PendingRegistration, Long> {
+    Optional<PendingRegistration> findByEmail(String email);
+    Optional<PendingRegistration> findByToken(String token);
+}
